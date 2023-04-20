0bc323904e27a62abf7de3bbfab4a9cf1a031b41
YH Huang <62240114+yhuangzl@users.noreply.github.com>
2023-04-20 18:41:10 -0400

    Update main.py

--- s23openalex/main.py
+++ s23openalex/main.py
@@ -5,5 +5,5 @@ from .works import Works
 @click.command(help='OpenAlex Institutions')
 @click.argument('query', nargs=-1)
 def main(query):
-    w = Works(query)
-    return w.ris
+    w = Works(query[0])
+    w.bibtex
