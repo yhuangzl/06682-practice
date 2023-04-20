71df8555245d8009b5d56fbd781e5afa3751afec
YH Huang <62240114+yhuangzl@users.noreply.github.com>
2023-04-20 17:01:50 -0400

    Create pkg folder and setup.py

---
+++ pkg/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='s23openalex',
+      version='0.0.1',
+      description='s23 openalex',
+      maintainer='Yu-Hsun Huang',
+      maintainer_email='yuhsunh@andrew.cmu.edu',
+      license='MIT',
+      packages=['s23openalex'],
+      long_description='''A long
+      multiline description.''')
